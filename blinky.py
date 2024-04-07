e72c1136551d94f1d7be6c7cd5e88bc1961f037e
PsiAmp <kostinkostya@gmail.com>
2024-04-07 19:30:14 +0300

    initial commit

---
+++ blinky.py
@@ -0,0 +1,44 @@
+from migen import *
+from litex.build.generic_platform import *
+from litex.build.lattice import LatticeECP5Platform
+from litex.build.lattice.programmer import EcpprogProgrammer
+
+# Platform definition
+_io = [
+    ("clk", 0, Pins("P2"), IOStandard("LVCMOS33")),
+    ("user_led", 0, Pins("P6"), IOStandard("LVCMOS33")),
+]
+
+class Platform(LatticeECP5Platform):
+    default_clk_name = "clk"
+    default_clk_period = 10.0
+
+    def __init__(self, board="i5", revision="7.0", toolchain="trellis"):
+        self.revision = revision
+        device     = {"7.0": "LFE5U-25F-6BG381C"}[revision]
+        io         = {"7.0": _io}[revision]
+        LatticeECP5Platform.__init__(self, device, io, toolchain=toolchain)
+
+    def create_programmer(self):
+        return EcpprogProgrammer()
+
+
+# Blinky design
+class Blinky(Module):
+    def __init__(self, platform):
+        self.clk = platform.request("clk")
+        self.led = platform.request("user_led")
+
+        platform.add_period_constraint(self.clk, 1e9 / 25e6)
+
+        self.clock_domains.cd_sys = ClockDomain()
+        self.comb += self.cd_sys.clk.eq(self.clk)
+
+        self.sync += self.led.eq(~self.led)
+
+# Build and program
+if __name__ == "__main__":
+    platform = Platform()
+    blinky = Blinky(platform)
+    platform.build(blinky)
+    # platform.create_programmer().load_bitstream("build/top.bin")
